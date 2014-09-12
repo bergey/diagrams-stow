b7b385f26cb3f337a8cdf17ca4dc3dc22235e69d
Daniel Bergey <bergey@alum.mit.edu>
2014-09-12 17:21:01 +0000

    update versions not yet in nixpkgs

--- nix/lib/shell.nix
+++ nix/lib/shell.nix
@@ -2,8 +2,10 @@ let pkgs = import <nixpkgs> {};
     haskellPackages = pkgs.haskellPackages.override {
       extension = self: super: {
         text = self.callPackage ../nix-updates/text/1.2.0.0.nix {};
-        parsec = self.callPackage ../nix-updates/parsec/3.1.6.nix {};
-        diagramsCore= self.callPackage ../core {};
+        lens = self.callPackage ../nix-updates/lens/4.4.0.2.nix {}; # needed for text-1.2
+        fsnotify = self.callPackage ../nix-updates/fsnotify/0.1.0.3.nix {};
+        optparseApplicative = self.callPackage ../nix-updates/optparse-applicative/0.10.0 {};
+        diagramsCore= self.callPackage ../../../core {};
         diagramsLib = self.callPackage ./. {};
       };
     };
