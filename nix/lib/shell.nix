18cd578c86c8a494d422fad41b3cd1658e7b4ba7
Daniel Bergey <bergey@alum.mit.edu>
2014-09-24 13:52:57 +0000

    nix: -lib depends on linear

--- nix/lib/shell.nix
+++ nix/lib/shell.nix
@@ -4,6 +4,7 @@ let pkgs = import <nixpkgs> {};
         text = self.callPackage ../nix-updates/text/1.2.0.0.nix {};
         lens = self.callPackage ../nix-updates/lens/4.4.0.2.nix {}; # needed for text-1.2
         fsnotify = self.callPackage ../nix-updates/fsnotify/0.1.0.3.nix {};
+        linear = self.callPackage ../../../linear {};
         optparseApplicative = self.callPackage ../nix-updates/optparse-applicative/0.10.0 {};
         diagramsCore= self.callPackage ../../../core {};
         diagramsLib = self.callPackage ./. {};
