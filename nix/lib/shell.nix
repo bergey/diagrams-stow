84b720730e74c84fbd0b89e5bef6bb16bdbd10c8
Daniel Bergey <bergey@alum.mit.edu>
2014-11-16 16:30:25 +0000

    WIP: shell.nix test

--- nix/lib/shell.nix
+++ nix/lib/shell.nix
@@ -1,16 +1,19 @@
-let pkgs = import <nixpkgs> {};
-    haskellPackages = pkgs.haskellPackages.override {
+{ pkgs ? import <nixpkgs> {}, haskellPackages ? pkgs.haskellPackages }:
+
+let
+     hs = haskellPackages.override {
       extension = self: super: {
         # required, not in Nix
-        fsnotify = self.callPackage /home/bergey/code/nixHaskellVersioned/fsnotify/0.1.0.3.nix {};
-        optparseApplicative = self.callPackage /home/bergey/code/nixHaskellVersioned/optparse-applicative/0.11.0.1.nix {};
-        # HEAD packages
-        monoidExtras = self.callPackage ../../../monoid-extras {};
-        active = self.callPackage ../../../active {};
-        diagramsCore= self.callPackage ../../../core {};
-        diagramsLib = self.callPackage ./. {};
+        # fsnotify = self.callPackage /home/bergey/code/nixHaskellVersioned/fsnotify/0.1.0.3.nix {};
+        # optparseApplicative = self.callPackage /home/bergey/code/nixHaskellVersioned/optparse-applicative/0.11.0.1.nix {};
+        # # newer than in Nix
+        # # HEAD packages
+        # monoidExtras = self.callPackage ../../../monoid-extras {};
+        # active = self.callPackage ../../../active {};
+        # diagramsCore= self.callPackage ../../../core {};
+        thisPackage = self.callPackage ./. {};
       };
     };
- in pkgs.lib.overrideDerivation haskellPackages.diagramsLib (attrs: {
-   buildInputs = [ haskellPackages.cabalInstall ] ++ attrs.buildInputs;
+in pkgs.lib.overrideDerivation hs.thisPackage (attrs: {
+            buildInputs = [hs.cabalInstall ] ++ attrs.buildInputs;
  })
