63c99a3ab16a82af07c69e3b1b261f19add900e8
Daniel Bergey <bergey@alum.mit.edu>
2014-11-20 19:34:03 +0000

    -builder, -haddock update for lens-4.6, HSE

--- nix/builder/shell.nix
+++ nix/builder/shell.nix
@@ -2,21 +2,16 @@
 
 let 
   tmpHaskellPkgs= haskellPackages.override {
-        extension = self: super: {
+        extension = self: super: rec {
+        hsPkg = pkg: version: self.callPackage "/home/bergey/code/nixHaskellVersioned/${pkg}/${version}.nix" {};
         # required, not in Nix
-        fsnotify = self.callPackage /home/bergey/code/nixHaskellVersioned/fsnotify/0.1.0.3.nix {};
-        optparseApplicative = self.callPackage /home/bergey/code/nixHaskellVersioned/optparse-applicative/0.11.0.1.nix {};
-        tasty = self.callPackage /home/bergey/code/nixHaskellVersioned/tasty/0.10.0.2.nix {};
-        haskellSrcExts = self.callPackage /home/bergey/code/nixHaskellVersioned/haskell-src-exts/1.16.0.nix {};
-        hlint= self.callPackage /home/bergey/code/nixHaskellVersioned/hlint/1.9.5.nix {};
-        lens = pkgs.lib.overrideDerivation super.lens (attrs: {
-              doCheck = false;
-            });
+        # newer than in Nix
+        lens = hsPkg "lens" "4.6";
         # HEAD packages        
-        monoidExtras = self.callPackage ../../../monoid-extras {};
-        active = self.callPackage ../../../active {};
-        diagramsCore = self.callPackage ../../../core {};
-        diagramsLib = self.callPackage ../../../lib {};
+        # monoidExtras = self.callPackage ../../../monoid-extras {};
+        # active = self.callPackage ../../../active {};
+        # diagramsCore = self.callPackage ../../../core {};
+        # diagramsLib = self.callPackage ../../../lib {};
         # self
         diagramsBuilder = self.callPackage ./. {};
       };
--- nix/haddock/shell.nix
+++ nix/haddock/shell.nix
@@ -1,30 +1,22 @@
 { pkgs ? import <nixpkgs> {}, haskellPackages ? pkgs.haskellPackages }:
 
 let 
-  tmpHaskellPkgs= haskellPackages.override {
-        extension = self: super: {
+  hs = haskellPackages.override {
+        extension = self: super: rec {
+        hsPkg = pkg: version: self.callPackage "/home/bergey/code/nixHaskellVersioned/${pkg}/${version}.nix" {};
+        lens = hsPkg "lens" "4.6";
         # required, not in Nix
-        fsnotify = self.callPackage /home/bergey/code/nixHaskellVersioned/fsnotify/0.1.0.3.nix {};
-        optparseApplicative = self.callPackage /home/bergey/code/nixHaskellVersioned/optparse-applicative/0.11.0.1.nix {};
-        tasty = self.callPackage /home/bergey/code/nixHaskellVersioned/tasty/0.10.0.2.nix {};
-        haskellSrcExts = self.callPackage /home/bergey/code/nixHaskellVersioned/haskell-src-exts/1.16.0.nix {};
-        hlint= self.callPackage /home/bergey/code/nixHaskellVersioned/hlint/1.9.5.nix {};
-        lens = pkgs.lib.overrideDerivation super.lens (attrs: {
-              doCheck = false;
-            });
         # HEAD packages
-        monoidExtras = self.callPackage ../../../monoid-extras {};
-        active = self.callPackage ../../../active {};
-        diagramsCore = self.callPackage ../../../core {};
-        diagramsLib = self.callPackage ../../../lib {};
-        diagramsSvg = self.callPackage ../../../svg {};
+        # monoidExtras = self.callPackage ../../../monoid-extras {};
+        # active = self.callPackage ../../../active {};
+        # diagramsCore = self.callPackage ../../../core {};
+        # diagramsLib = self.callPackage ../../../lib {};
+        # diagramsSvg = self.callPackage ../../../svg {};
         diagramsBuilder = self.callPackage ../../../builder {};
         # self
         diagramsHaddock= self.callPackage ./. {};
       };
     };
-  in let
-     haskellPackages = tmpHaskellPkgs;
-     in pkgs.lib.overrideDerivation haskellPackages.diagramsHaddock (attrs: {
-       buildInputs = [ haskellPackages.cabalInstall ] ++ attrs.buildInputs;
+         in pkgs.lib.overrideDerivation hs.diagramsHaddock (attrs: {
+                buildInputs = [hs.cabalInstall ] ++ attrs.buildInputs;
  })
