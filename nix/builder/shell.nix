b424cd13a30bf5df253a142c4678c5e965a23e10
Daniel Bergey <bergey@alum.mit.edu>
2014-11-24 19:46:24 +0000

    nix: add deps for builder executables to -builder

--- nix/builder/shell.nix
+++ nix/builder/shell.nix
@@ -6,12 +6,17 @@ let
         hsPkg = pkg: version: self.callPackage "/home/bergey/code/nixHaskellVersioned/${pkg}/${version}.nix" {};
         # required, not in Nix
         # newer versions
+        # adjunctions = hsPkg "adjunctions" "4.0";
+        # contravariant = hsPkg "contravariant" "0.6.1.1";
         lens = hsPkg "lens" "4.6";
         # HEAD packages        
         monoidExtras = self.callPackage ../../../monoid-extras {};
         active = self.callPackage ../../../active {};
         diagramsCore = self.callPackage ../../../core {};
         diagramsLib = self.callPackage ../../../lib {};
+        diagramsCairo = self.callPackage ../../../cairo {};
+        diagramsPostscript = self.callPackage ../../../postscript {};
+        diagramsSvg = self.callPackage ../../../svg {};
         # self
         diagramsBuilder = self.callPackage ./. {};
       };
