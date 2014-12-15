f903ebac368935cfb380016cd9abf7acb546d8ff
Daniel Bergey <bergey@alum.mit.edu>
2014-12-15 20:05:02 +0000

    cairo: use latest lens

--- nix/cairo/shell.nix
+++ nix/cairo/shell.nix
@@ -5,10 +5,10 @@ let
     extension = self: super: rec {
         hsPkg = pkg: version: self.callPackage "/home/bergey/code/nixHaskellVersioned/${pkg}/${version}.nix" {};
         # required, not in Nix
-        fsnotify = hsPkg "fsnotify" "0.1.0.3";
-        optparseApplicative = hsPkg "optparse-applicative" "0.11.0.1";
+        # fsnotify = hsPkg "fsnotify" "0.1.0.3";
+        # optparseApplicative = hsPkg "optparse-applicative" "0.11.0.1";
         # newer than Nix
-        lens = hsPkg "lens" "4.6";
+        # lens = hsPkg "lens" "4.6";
         # HEAD packages
         # monoidExtras = self.callPackage ../../../monoid-extras {};
         # active = self.callPackage ../../../active {};
