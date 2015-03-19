4c6cc2b78962a892122d21b065694a167c316090
Daniel Bergey <bergey@alum.mit.edu>
2015-03-19 16:16:33 +0000

    statestack uses transformers-compat now

--- nix/statestack/default.nix
+++ nix/statestack/default.nix
@@ -1,9 +1,11 @@
-{ mkDerivation, base, mtl, stdenv, transformers }:
+{ mkDerivation, base, mtl, stdenv, transformers
+, transformers-compat
+}:
 mkDerivation {
   pname = "statestack";
   version = "0.2.0.3";
   src = ./.;
-  buildDepends = [ base mtl transformers ];
+  buildDepends = [ base mtl transformers transformers-compat ];
   description = "Simple State-like monad transformer with saveable and restorable state";
   license = stdenv.lib.licenses.bsd3;
 }
