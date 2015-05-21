f848e43756e26f4a78fb2a0c4ffdfb4f5e9f31a0
Daniel Bergey <bergey@alum.mit.edu>
2015-05-21 19:00:37 +0000

    add more packages to packdeps

--- packdeps/packdeps.sh
+++ packdeps/packdeps.sh
@@ -1,3 +1,3 @@
- for pkg in active core lib contrib statestack svg cairo postscript rasterific canvas SVGFonts builder haddock docutils pandoc povray; do
+ for pkg in monoid-extras dual-tree active force-layout palette solve statestack SVGFonts core lib contrib svg cairo postscript rasterific canvas builder haddock docutils pandoc povray; do
      packdeps $pkg/*.cabal
 done
