41961965431601eee4b44b7d918ce2f719646091
Daniel Bergey <bergey@teallabs.org>
2015-11-17 17:05:50 -0500

    packdeps: check diagrams-ghcjs

--- packdeps/packdeps.sh
+++ packdeps/packdeps.sh
@@ -1,3 +1,3 @@
- for pkg in  monoid-extras dual-tree core active solve lib SVGFonts palette force-layout contrib statestack cairo gtk postscript rasterific svg canvas html5 diagrams builder haddock; do
+ for pkg in  monoid-extras dual-tree core active solve lib SVGFonts palette force-layout contrib statestack cairo gtk postscript rasterific svg canvas html5  builder haddock ghcjs/diagrams-ghcjs; do
      packdeps $pkg/*.cabal
 done
